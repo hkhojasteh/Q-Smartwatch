9984612bd6372362978d39a62764c924fc36ee35
Hadi Abdi Khojasteh <khojasteh.programmer@gmail.com>
2018-03-02 03:03:39 +0330

    init.lua changed

--- Scripts/init.lua
+++ Scripts/init.lua
@@ -1,1 +1,10 @@
-print("I'm Ready!")
+uart.on("data", "\r",
+  function(data)
+    print("receive from uart:", data)
+    node.input(data)
+    tm = rtctime.epoch2cal(rtctime.get())
+    print(string.format("%04d/%02d/%02d %02d:%02d:%02d", tm["year"], tm["mon"], tm["day"], tm["hour"], tm["min"], tm["sec"]))
+    if data=="quit\r" then
+      uart.on("data") -- unregister callback function
+    end
+end, 0)
