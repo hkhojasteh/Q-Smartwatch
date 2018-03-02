7d771ac4f53e4d00759df9c1bd0f8c444c75cf6b
Hadi Abdi Khojasteh <khojasteh.programmer@gmail.com>
2018-03-02 04:10:10 +0330

    init.lua startup tester

--- Scripts/init.lua
+++ Scripts/init.lua
@@ -1,10 +1,11 @@
 uart.on("data", "\r",
   function(data)
-    print("receive from uart:", data)
+    print("?:", data)
     node.input(data)
     tm = rtctime.epoch2cal(rtctime.get())
     print(string.format("%04d/%02d/%02d %02d:%02d:%02d", tm["year"], tm["mon"], tm["day"], tm["hour"], tm["min"], tm["sec"]))
-    if data=="quit\r" then
-      uart.on("data") -- unregister callback function
+    if tostring(data) == "quit" then
+        print("Goodbye ...")
+        uart.on("data") -- unregister callback function
     end
-end, 0)
+end, 1)
